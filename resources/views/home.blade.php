d6a5ed38fdf0ed0b15704ceda0cc0ca16db53557
Amauryrdzrz <amauryrdzrz@gmail.com>
2023-04-25 22:40:40 -0600

    prueba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,26 +33,28 @@
         </div>
     </div>
 </div> -->
-<div class="container">
-    @foreach ($discos as $disco)
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="{{ $disco->archivo }}" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">Nombre: {{$disco->nombre}}</h5>
-            <p class="card-text">Nacionalidad: {{$disco->categoria}}**</p>
-            <p class="card-text">Categoria: {{$disco->cantante}}**</p>
-            <p class="card-text">Precio: ${{$disco->precio}}</p>
-            <div style="display:flex; flex-direction:row;">
-                <form action="{{ route('edit',$disco->id) }}" method="GET">
-                    @csrf
-                    @method('GET')
-                    <button class="btn btn-sm btn-success" type="submit"><i class="fa fa-fw fa-edit"></i>Editar</button>
-                </form>
-                <form action="{{ route('delete',$disco->id)}}" method="GET">
-                    @csrf
-                    @method('GET')
-                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i>Eliminar</button>
-                </form>
+<div class="row">
+    <div class="col-sm-6">
+        @foreach ($discos as $disco)
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="{{ $disco->archivo }}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Nombre: {{$disco->nombre}}</h5>
+                <p class="card-text">Nacionalidad: {{$disco->categoria}}**</p>
+                <p class="card-text">Categoria: {{$disco->cantante}}**</p>
+                <p class="card-text">Precio: ${{$disco->precio}}</p>
+                <div style="display:flex; flex-direction:row;">
+                    <form action="{{ route('edit',$disco->id) }}" method="GET">
+                        @csrf
+                        @method('GET')
+                        <button class="btn btn-sm btn-success" type="submit"><i class="fa fa-fw fa-edit"></i>Editar</button>
+                    </form>
+                    <form action="{{ route('delete',$disco->id)}}" method="GET">
+                        @csrf
+                        @method('GET')
+                        <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i>Eliminar</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
