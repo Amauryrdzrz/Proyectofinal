3c881333f96e6eca36ee713e18c693fe2897fa27
Amauryrdzrz <amauryrdzrz@gmail.com>
2023-04-26 17:01:22 -0600

    edit

--- resources/views/disco/edit.blade.php
+++ resources/views/disco/edit.blade.php
@@ -5,84 +5,6 @@
 @endsection
 
 @section('content')
-    <!-- <section class="content container-fluid">
-        <div class="">
-            <div class="col-md-12">
-                @includeif('partials.errors')
-                <div class="card card-default">
-                    <div class="card-header">
-                        <span class="card-title">Actualizar Platillo</span>
-                    </div>
-                    <div class="card-body">
-
-                      <div class="row">
-                        <div class="col-md-5"></div>
-                        <div class="col-md-2"><img width=60% height=60% src="{{$disco->archivo}}"></div>
-                        <div class="col-md-5"></div>
-                      </div>
-                      <form  method="POST" action="{{ route('discos.update', $disco->id) }}"  enctype="multipart/form-data">
-                         @csrf
-                         @method('PUT')
-                        <div class="row">
-                          <div class="col-md-3"></div>
-                          <div class="form-group col-md-4">
-                            <label for="nombre">Platillo</label>
-                            <input type="text" value="{{$disco->nombre}}" name="nombre" id="nombre" class="form-control" required>
-                          </div>
-
-                          <div class="form-group col-md-2">
-                            <label for="categoria">Nacionalidad</label>
-                            <input value="{{$disco->categoria}}" name="categoria" id="categoria" class="form-control" required>
-                          </div>
-                          <div class="col-md-3"></div>
-                        </div>
-
-                        <div class="row">
-                          <div class="col-md-3"></div>
-                            <div class="form-group col-md-3">
-                              <label for="cantante">Categoria</label>
-                              <input type="text"  value="{{$disco->cantante}}" name="cantante" id="cantante" class="form-control" required>
-                            </div>
-                            <div class="form-group col-md-2">
-                              <label for="precio">Precio</label>
-                              <input type="text" class="form-control" value="{{$disco->precio}}" name="precio" id="precio" required>
-                            </div>
-                          <div class="col-md-4"></div>
-                        </div>
-                        
-                        <div class="row">
-                          <div class="col-md-3"></div>
-                          <div class="form-group col-md -4">
-                            <label for="image">Imagen descriptiva</label>
-                            <input type="file" value="{{$disco->archivo}}" name="image" id="image" class="form-control" required>
-                          </div>
-                          <div class="col-md-3"></div>
-                        </div>
-                        <br>
-
-                        <div class="row">
-                          <div class="col-md-3"></div>
-                             @if (Auth::user()->area == 1)
-                                <div class="form-group col-md -4">
-                                  <label for="codigoS">Código para editar.</label>
-                                  <input type="number" value="" name="codigoS" id="codigoS" class="form-control" required>
-                                  <a href="/solicitud?accion=EDITAR">Solicitar Código </a>
-                                </div>
-                            @endif
-                            <div class="form-group col-md-2">
-                            <br>
-                            <button type="submit" class="btn btn-primary">Actualizar</button>
-                            </div>
-                          <div class="col-md-4"></div>
-                        </div>
-
-                      </form>
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> -->
 
 <div class="col-sm-12">
             <div class="card" style="left:25vw; width: 50%;">
@@ -149,46 +71,6 @@
                 </div>
             </div>
         </div>
-
-    <!-- <form method="POST" action="{{ route('discos.update', $disco->id) }}"  enctype="multipart/form-data">
-                            @csrf
-                            @method('PUT')
-                            <div class="box box-info padding-1">
-                                <div class="box-body"> 
-                                        <label for="nombre">
-                                            Nombre
-                                        </label>
-                                        <input type="text" value="{{$disco->nombre}}" name="nombre" id="nombre">
-                                        <label for="categoria">
-                                            Categoria
-                                        </label>
-                                        <input type="text" value="{{$disco->categoria}}" name="categoria" id="categoria">
-                                        <label for="cantante">
-                                            Cantante
-                                        </label>
-                                        <input type="text" value="{{$disco->cantante}}" name="cantante" id="cantante">
-                                        <label for="precio">
-                                            Precio
-                                        </label>
-                                        <input type="text" value="{{$disco->precio}}" name="precio" id="precio">
-                                        <label for="archivo">
-                                            Archivo
-                                        </label>
-                                        <input type="file" value="{{$disco->archivo}}" name="image" id="image">
-
-
-                                        @if (Auth::user()->area == 1)
-                                           <label for="archivo">
-                                              Codigo De seguridad
-                                           </label>
-                                           <input type="hidden" value="ELIMINAR" name="accion" codigo="accion">
-                                           <input type="number" value="" name="codigoS" id="codigoS" required>
-                                           <a href="/solicitud?accion=EDITAR"> Solicitar codigo de utilidad... </a>
-                                        @endif
-                                        <button type="submit" class="btn btn-primary">Actualizar</button>
-                                </div>
-                            </div>
-                        </form> -->
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
