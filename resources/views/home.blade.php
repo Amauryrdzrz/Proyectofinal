6b6d7247be0107164933cc60a10c19b92d48805c
Amauryrdzrz <amauryrdzrz@gmail.com>
2023-04-25 22:17:15 -0600

    prueba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,27 +32,27 @@
             </div>
         </div>
         <div class="card-body">
-                        <div class="card">
-                            @foreach ($discos as $disco)
-                            <img class="card-img-top" style="width:20%; height:20%;" src="{{ $disco->archivo }}" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">Nombre: {{$disco->nombre}}</h5>
-                                <p class="card-text">Nacionalidad: {{$disco->categoria}}**</p>
-                                <p class="card-text">Categoria: {{$disco->cantante}}**</p>
-                                <p class="card-text">Precio: ${{$disco->precio}}</p>
-                                <div style="display:flex; flex-direction:row;">
-                                <form action="{{ route('edit',$disco->id) }}" method="GET">
-                                    @csrf
-                                    @method('GET')
-                                    <button class="btn btn-sm btn-success" type="submit"><i class="fa fa-fw fa-edit"></i>Editar</button>
-                                </form>
-                                <form action="{{ route('delete',$disco->id)}}" method="GET">
-                                    @csrf
-                                    @method('GET')
-                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i>Eliminar</button>
-                                </form>
-                                </div>
-                            </div>
+            <div class="card">
+                @foreach ($discos as $disco)
+                    <img class="card-img-top" style="width:20%; height:20%;" src="{{ $disco->archivo }}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Nombre: {{$disco->nombre}}</h5>
+                            <p class="card-text">Nacionalidad: {{$disco->categoria}}**</p>
+                            <p class="card-text">Categoria: {{$disco->cantante}}**</p>
+                            <p class="card-text">Precio: ${{$disco->precio}}</p>
+                        <div style="display:flex; flex-direction:row;">
+                            <form action="{{ route('edit',$disco->id) }}" method="GET">
+                                @csrf
+                                @method('GET')
+                                <button class="btn btn-sm btn-success" type="submit"><i class="fa fa-fw fa-edit"></i>Editar</button>
+                            </form>
+                            <form action="{{ route('delete',$disco->id)}}" method="GET">
+                                @csrf
+                                @method('GET')
+                                <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i>Eliminar</button>
+                            </form>
+                        </div>
+                        </div>
                         </div>
                         @endforeach
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,12 +43,11 @@
                                 </li> -->
                             @endif
                         @else
-                        <!-- <li class="nav-item">
-                            <a class="nav-link" href=" {{ route('discos.index') }}"> Platillos</a>
-</li> -->
-<li class="nav-item">
-                          
-</li>
+                            @if (Auth::user()->area >= 2)
+                            <li class="nav-item">
+                                <a class="nav-link" href=" {{ route('solicita') }}"> Solicitudes Pendientes</a>
+                            </li>
+                            @endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
